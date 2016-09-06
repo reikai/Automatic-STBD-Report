bbb10842fd2224f46ae64e73948d09a2a2956921
reikai <creativepsp@gmail.com>
2016-09-06 13:40:59 -0500

    Add files via upload

--- STBDreport.py
+++ STBDreport.py
@@ -3,16 +3,16 @@
 
 import pandas as pd
 import numpy as np
-from selenium import webdriver
+
 from datetime import datetime
 import os
 import time
 
 # Email variables. To List, Subject Line, Message body.
-ToList = ['ty.tippawang@vizientinc.com']
+ToList = ['ty.tippawang@vizientinc.com', 'creativepsp@gmail.com']
 SubjectLine = 'Reporting Test'
 MessageBody = '''
-A new L-Vizient report has just been run 
+A new L-Vizient report has just been run
 '''
 
 # This is where the file gets downloaded. chdir to the folder, get the latest file.
@@ -32,39 +32,8 @@ df = df.drop(df.index([[-1]]))        # Eliminate the last row.
 # TODO: Save the file in a team location.
 
 # TODO: Log on to Gmail and email the file out.
-def GmailLogin():
-    # Define credentials
-    username = 'practicingpython006@gmail.com'
-    password = 'practicingpython'
-
-    # Open Gmail in Firefox
-    browser = webdriver.Firefox()
-    browser.get('https://gmail.com')
-    print('Opening Gmail...')
-
-    # Select login field, input username
-    print('Attempting to login...')
-    LoginElem = browser.find_element_by_id('Email')
-    LoginElem.send_keys(username)
-
-    # Click Next to unlock/unhide the password field
-    NextButton = browser.find_element_by_id('next')
-    NextButton.click()
-
-    # Select Password field, input password
-    PassElem = browser.find_element_by_id('Passwd')
-    PassElem.send_keys(password)
-
-    # Click Sign in button, wait 5 seconds.
-    print('Username & Password have been inputted. Signing in...')
-    SignInButton = browser.find_element_by_id('signIn')
-    SignInButton.click()
-    time.sleep(5)         # This way, the program doesn't execute faster than a slow internet connection
-
-def Compose(toList, subjectText, bodyText):
-    # Click compose to cause the in-browser popup
-    ComposeButton = browser.find_element_by_class_name('z0')
-    ComposeButton.click()
 
 
-# TODO: Close the browser, logging out of everything. (Closing a Selenium browser is the equivalent of logging off.)
+
+# TODO: Close the browser, logging out of everything.
+
