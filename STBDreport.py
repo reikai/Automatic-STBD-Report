9d9101405a1c662f23b273993f661e57a3961f60
reikai <creativepsp@gmail.com>
2016-09-06 10:11:32 -0500

    Add files via upload

--- STBDreport.py
+++ STBDreport.py
@@ -1,47 +1,70 @@
 #! python3
 # Generates fresh STBD report, saves it to share drive, and emails interested parties.
 
+import pandas as pd
+import numpy as np
 from selenium import webdriver
+from datetime import datetime
+import os
+import time
 
-URL = 'https://adfs.alliancewebs.net/adfs/ls/?wa=wsignin1.0&wtrealm=http%3a%2f%2fallianceprod.cloud.infor.com%2fadfs%2fservices%2ftrust&wctx=5aa26fcd-2991-41bc-b9ff-2e34216ebadc'
-URL2 = 'https://allianceprod.cloud.infor.com/LawsonBI/SitePages/Default.aspx'
+# Email variables. To List, Subject Line, Message body.
+ToList = ['ty.tippawang@vizientinc.com']
+SubjectLine = 'Reporting Test'
+MessageBody = '''
+A new L-Vizient report has just been run 
+'''
 
-# Log on to Lawson/LBI/whatever.
-username = r'VHACORP\ttippawa'
-password = input('Please enter Ty\'s password.')
-browser = webdriver.Firefox()
-browser.get(URL)
+# This is where the file gets downloaded. chdir to the folder, get the latest file.
+DLlocation = r'C:\Python Practice 3\Reporting Practice'
+os.chdir(DLlocation)
+files = [x for x in os.listdir('.') if x.endswith('.xlsx')]
+newest = max(files, key=os.path.getctime)  # newest = the latest modified file
 
-# Input the username.
-print('Inputting username...')
-usernameElem = browser.find_element_by_id('ctl00_ContentPlaceHolder1_UsernameTextBox')
-usernameElem.send_keys(username)
-print('Username has been inputted.')
+df = pd.read_excel(newest)            # Read in Excel file into Pandas Dataframe.
 
-# Input the password.
-print('Inputting password...')
-passwordElem = browser.find_element_by_id('ctl00_ContentPlaceHolder1_PasswordTextBox')
-passwordElem.send_keys(password)
-print('Password has been inputted.')
+df.columns = df.iloc[0,:]             # Overwrite the columns to be the first row.
+df = df.drop(df.index([[0]]))         # Eliminate the first row.
+df = df.drop(df.index([[-1]]))        # Eliminate the last row.
 
-# Sign in once. This shouldn't work, based on testing.
-print('Login attempt one.')
-signInButtonElem = browser.find_element_by_id('ctl00_ContentPlaceHolder1_SubmitButton')
-signInButtonElem.click()
-print('Login attempt one completed.')
+# TODO: Leave the original data on one tab, but also make a Pivot Table on another tab.
 
-# Sign in again.
-print('Login attempt 2. If this fails, end the program.')
-browser.get(URL2)
-continueToSignInElem = browser.find_element_by_id('ctl00_ContentPlaceHolder1_PassiveSignInButton')
-continueToSignInElem.click()
+# TODO: Save the file in a team location.
 
-# TODO: Navigate the menus to the STBD report. Click the report and cause the popup.
+# TODO: Log on to Gmail and email the file out.
+def GmailLogin():
+    # Define credentials
+    username = 'practicingpython006@gmail.com'
+    password = 'practicingpython'
+
+    # Open Gmail in Firefox
+    browser = webdriver.Firefox()
+    browser.get('https://gmail.com')
+    print('Opening Gmail...')
+
+    # Select login field, input username
+    print('Attempting to login...')
+    LoginElem = browser.find_element_by_id('Email')
+    LoginElem.send_keys(username)
+
+    # Click Next to unlock/unhide the password field
+    NextButton = browser.find_element_by_id('next')
+    NextButton.click()
+
+    # Select Password field, input password
+    PassElem = browser.find_element_by_id('Passwd')
+    PassElem.send_keys(password)
+
+    # Click Sign in button, wait 5 seconds.
+    print('Username & Password have been inputted. Signing in...')
+    SignInButton = browser.find_element_by_id('signIn')
+    SignInButton.click()
+    time.sleep(5)         # This way, the program doesn't execute faster than a slow internet connection
+
+def Compose(toList, subjectText, bodyText):
+    # Click compose to cause the in-browser popup
+    ComposeButton = browser.find_element_by_class_name('z0')
+    ComposeButton.click()
 
 
-# TODO:
-# TODO:
-# TODO:
-# TODO:
-# TODO:
-# TODO:
+# TODO: Close the browser, logging out of everything. (Closing a Selenium browser is the equivalent of logging off.)
